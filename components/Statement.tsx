8c7fe63eff5dadb1d7fda8e3adeb4c31d131c3ab
shubham-webileapps <101321655+shubham-webileapps@users.noreply.github.com>
2022-09-10 16:53:31 +0530

    1 file modified

--- components/Statement.tsx
+++ components/Statement.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 // import { Link } from 'react-router-dom';
 import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -10,7 +14,7 @@ import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 import { useSelector } from 'react-redux';
 import { Link as RouterLink } from 'react-router-dom';
-import Link from '@mui/material/Link';
+import Button from '@mui/material/Button';
 
 interface Column {
   id: 'a' | 'date' | 'mode' | 'amount' | 'total';
@@ -88,54 +92,77 @@ export default function Statement() {
 
   return (
     <Paper sx={{ width: '100%' }}>
-      <TableContainer sx={{ maxHeight: 440 }}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              {columns.map((column) => (
-                <TableCell key={column.id} style={{ minWidth: 10 }}>
-                  {column.label}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row) => {
-                return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                    {columns.map((column) => {
-                      const value = row[column.id];
+      <Box>
+        <Card>
+          <CardContent>
+            <TableContainer sx={{ maxHeight: 440 }}>
+              <Table stickyHeader aria-label="sticky table">
+                <TableHead>
+                  <TableRow>
+                    {columns.map((column) => (
+                      <TableCell key={column.id} style={{ minWidth: 10 }}>
+                        {column.label}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {rows
+                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                    .map((row) => {
                       return (
-                        <TableCell key={column.id} align={column.align}>
-                          {column.format && typeof value === 'number'
-                            ? column.format(value)
-                            : value}
-                        </TableCell>
+                        <TableRow
+                          hover
+                          role="checkbox"
+                          tabIndex={-1}
+                          key={row.code}
+                        >
+                          {columns.map((column) => {
+                            const value = row[column.id];
+                            return (
+                              <TableCell key={column.id} align={column.align}>
+                                {column.format && typeof value === 'number'
+                                  ? column.format(value)
+                                  : value}
+                              </TableCell>
+                            );
+                          })}
+                        </TableRow>
                       );
                     })}
-                  </TableRow>
-                );
-              })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
-      <Link component={RouterLink} className="mx-2" to="/Deposit">
-        Deposit
-      </Link>
-      <Link component={RouterLink} className="mx-2" to="/Withdraw">
-        Withdraw
-      </Link>
+                </TableBody>
+              </Table>
+            </TableContainer>
+            <TablePagination
+              rowsPerPageOptions={[10, 25, 100]}
+              component="div"
+              count={rows.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+          </CardContent>
+          <CardActions>
+            <Button
+              component={RouterLink}
+              size="small"
+              sx={{ color: 'red' }}
+              to="/Deposit"
+            >
+              Deposit
+            </Button>
+            <Button
+              component={RouterLink}
+              size="small"
+              sx={{ color: 'red' }}
+              to="/Withdraw"
+            >
+              Withdraw
+            </Button>
+          </CardActions>
+        </Card>
+      </Box>
     </Paper>
   );
 }
