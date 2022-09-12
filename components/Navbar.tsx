db4a32073eb783cc6d23c82225602756ab92cb7b
shubham-webileapps <101321655+shubham-webileapps@users.noreply.github.com>
2022-09-12 13:02:16 +0530

    1 file modified

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,83 +1,12 @@
-import * as React from 'react';
+import React from 'react';
 import { AppBar, Button, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
-
-const Navbar = () => {
-  return (
-    <AppBar position="static">
-      <Button
-        // className="navbar-brand d-flex justify-content-start"
-        component={Link}
-        to="/"
-      >
-        <AccountBalanceIcon
-          sx={{
-            display: { xs: 'none', color: 'white', md: 'flex' },
-            mr: 1,
-          }}
-        />
-
-        <Typography
-          variant="h6"
-          noWrap
-          sx={{
-            mr: 2,
-            display: { xs: 'none', md: 'flex', color: 'white' },
-            hover: { color: 'white' },
-            fontFamily: 'monospace',
-            fontWeight: 700,
-            letterSpacing: '.3rem',
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          My Bank
-        </Typography>
-        {/* </Button>
-      <Button
-        className="navbar-brand d-flex justify-content-start"
-        component={Link}
-        to="/"
-      > */}
-        <AccountBalanceIcon
-          sx={{
-            display: { xs: 'flex', md: 'none', color: 'white' },
-            mr: 1,
-          }}
-        />
-
-        <Typography
-          variant="h5"
-          noWrap
-          sx={{
-            mr: 2,
-            display: { xs: 'flex', color: 'white', md: 'none' },
-            hover: { color: 'white' },
-            flexGrow: 1,
-            fontFamily: 'monospace',
-            fontWeight: 700,
-            letterSpacing: '.3rem',
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          My Bank
-        </Typography>
-      </Button>
-    </AppBar>
-  );
-};
-export default Navbar;
-
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 
-export default function DenseAppBar() {
+export default function Navbar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
